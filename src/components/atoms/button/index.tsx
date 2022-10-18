8854926848316031fcf4885b28546bbd8efe5aa7
jimmyjamieson <ianjamieson@greenjade.com>
2022-10-18 16:04:54 +0200

    hmm

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -1,5 +1,5 @@
 import { component$, Slot } from '@builder.io/qwik';
-import { css } from '@stitches/core';
+import { css } from '~/theme/config';
 
 export const style = css({ background: '#006ce9' });
 export const Button: any = component$(() => (
