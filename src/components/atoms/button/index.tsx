8a9d14bbe0da91a7e4232fce429c8c58ed6affd5
jimmyjamieson <ianjamieson@greenjade.com>
2022-10-18 16:19:14 +0200

    button test

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -5,7 +5,8 @@ export const Button = component$(() => {
   const buttonStyle = css({
     backgroundColor: 'red',
   });
+  console.log('buttonStyle', buttonStyle);
   return (
-    <button class={buttonStyle()}><Slot /></button>
+    <button class={buttonStyle().className}><Slot /></button>
   );
 });
