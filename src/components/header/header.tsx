86cc303b3939c4c6ab2e9d2cad2000dd61f37a56
jimmyjamieson <ianjamieson@greenjade.com>
2022-10-18 16:22:30 +0200

    button test

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,11 @@
-import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import { component$ } from '@builder.io/qwik';
 import { Link } from '@builder.io/qwik-city';
-import styles from './header.css?inline';
+// import styles from './header.css?inline';
 import { getLocale } from '~/utils/routing/getLocale';
 import { config } from '~/config';
 
 export default component$(() => {
-  useStylesScoped$(styles);
+  // useStylesScoped$(styles);
   const locale = getLocale();
   const path = locale === config.defaultLocale ? '' : locale;
 
