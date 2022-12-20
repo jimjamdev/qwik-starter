2fb9c2029737a8a500b0d5ae205fc22edc60166a
Ian Jamieson <cogocreative@gmail.com>
2022-12-20 15:39:54 +0100

    test vanilla extract

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,19 +1,29 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useClientEffect$, useSignal, useStore } from "@builder.io/qwik";
 import { Link } from '@builder.io/qwik-city';
 import { getLocale } from '~/utils/routing/getLocale';
 import { config } from '~/config';
 import { headerCss } from "~/components/header/header.css";
 
 export default component$(() => {
-  // useStylesScoped$(styles);
   const locale = getLocale();
   const path = locale === config.defaultLocale ? '/' : `/${locale}/`;
+  const ref = useSignal<Element>();
+  const store = useStore({
+    height: 0,
+  });
+  useClientEffect$(() => {
+    if (ref.value) {
+      const rect = ref.value.getBoundingClientRect();
+      store.height = Math.round(rect.height);
+    }
+  });
 
   return (
-    <nav class={headerCss}>
+    <nav class={headerCss} ref={ref}>
       <Link href={`${path}`} aria-current="page">Home</Link>
 
       <Link href={`${path}listing`}>Listing</Link>
+      {store.height}
     </nav>
   );
 });
