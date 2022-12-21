1922dd3dbb08e58659ce26f5840a265a557a1d1a
Ian Jamieson <cogocreative@gmail.com>
2022-12-21 13:24:46 +0100

    test fetch init

---
+++ src/data/users/fetchUsers.ts
@@ -0,0 +1,13 @@
+import type { RequestHandler } from '@builder.io/qwik-city';
+
+export interface Users {
+  name: string;
+  height: number;
+  gender: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const fetchUsers: RequestHandler<Users> = async ({ params }) => {
+  const req = await fetch('https://swapi.dev/api/people/1/');
+  return req.json()
+};
--- src/routes/[...index]/index.tsx
+++ src/routes/[...index]/index.tsx
@@ -1,14 +1,25 @@
-import { component$ } from '@builder.io/qwik';
-import { DocumentHead } from '@builder.io/qwik-city';
+import { component$, Resource } from "@builder.io/qwik";
+import { DocumentHead, useEndpoint } from "@builder.io/qwik-city";
 import { useLocale } from "~/context/locale";
 import { Button } from "~/ui/components/button";
+import { Users } from "~/data/users/fetchUsers";
 
 export default component$(() => {
   const { lang } = useLocale();
-  console.log('locale', lang);
-  return (<div>locale route {lang} <Button>Test</Button></div>);
+  const users = useEndpoint<Users>();
+  return (
+    <>
+      <div>
+        locale route {lang} <Button>Test</Button>
+      </div>
+      <Resource
+        value={users}
+        onResolved={(users) => <>{JSON.stringify(users, null, 2)}</>}
+      />
+    </>
+  );
 });
 
 export const head: DocumentHead = {
-  title: 'Casino App',
+  title: "Casino App",
 };
