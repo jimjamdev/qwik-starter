6d756b54649dd3c7a3704362c3d534283e6f23f3
Ian Jamieson <cogocreative@gmail.com>
2022-12-21 12:44:14 +0100

    theme switch

--- src/root.tsx
+++ src/root.tsx
@@ -1,4 +1,4 @@
-import { component$, useStore } from "@builder.io/qwik";
+import { component$, useStore, $ } from "@builder.io/qwik";
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from "@builder.io/qwik-city";
 import { RouterHead } from './components/router-head/router-head';
 import { darkTheme } from "~/ui/theme/dark.css";
@@ -10,17 +10,17 @@ export default component$(() => {
   const theme = useStore({
     value: darkTheme,
   });
-  /*const switchTheme = () => {
+  const switchTheme = $(() => {
     theme.value = theme.value === darkTheme ? lightTheme : darkTheme;
-  };*/
+  });
   return (
     <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
         <RouterHead />
       </head>
-      <body lang="en" class={theme.value}>
-      <button onClick$={() => theme.value = theme.value === darkTheme ? lightTheme : darkTheme}>switch</button>
+      <body class={theme.value}>
+      <button onClick$={switchTheme}>switch</button>
       <LocaleProvider>
        <RouterOutlet />
       </LocaleProvider>
