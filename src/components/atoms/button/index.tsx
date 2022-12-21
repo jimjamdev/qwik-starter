42bb810f7df9f099e72800641eaf15be1c7d29ef
Ian Jamieson <cogocreative@gmail.com>
2022-12-21 10:45:40 +0100

    delete atoms

--- src/components/atoms/button/index.tsx
+++
@@ -1,7 +0,0 @@
-import { component$, Slot } from '@builder.io/qwik';
-
-export const Button = component$(() => {
-  return (
-    <button class="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500"><Slot /></button>
-  );
-});
