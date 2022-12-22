9846d8fcef12e55408cf46a7ddc0569c85bcb599
Ian Jamieson <cogocreative@gmail.com>
2022-12-22 16:23:48 +0100

    footerComponent

--- src/routes/[...index]/layout.tsx
+++ src/routes/[...index]/layout.tsx
@@ -2,19 +2,23 @@ import { component$, Slot } from "@builder.io/qwik";
 import { DefaultLayout } from "~/ui/layouts/default";
 
 export const Header = component$(() => {
-  return (
-      <div>Header</div>
-  );
+  return <div>Header</div>;
 });
 
 export const SideBar = component$(() => {
-  return (
-    <div>Side</div>
-  );
+  return <div>Side</div>;
+});
+
+export const Footer = component$(() => {
+  return <div>footer</div>;
 });
 
 export default component$(() => (
-  <DefaultLayout headerComponent={Header} sidebarComponent={SideBar}>
+  <DefaultLayout
+    headerComponent={Header}
+    sidebarComponent={SideBar}
+    footerComponent={Footer}
+  >
     <Slot />
   </DefaultLayout>
 ));
