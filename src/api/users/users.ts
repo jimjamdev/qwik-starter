a6112a51aedbb1a08157e68eedad74a5812fcbdc
Ian Jamieson <cogocreative@gmail.com>
2023-01-09 15:00:27 +0100

    params type

--- src/api/users/users.ts
+++ src/api/users/users.ts
@@ -9,8 +9,13 @@ export interface Users {
 export interface IP {
   ip: string;
 }
+
+export interface Params {
+  limit?: number;
+  page?: number;
+}
 //Test fetch
-export const fetchUsers = async (params: { limit: number, page: number }): Promise<Users> => {
+export const fetchUsers = async (params: Params): Promise<Users> => {
   const { limit = 5, page = 1 } = params || {};
   const req = await fetch(`https://catfact.ninja/breeds?limit=${limit}&page=${page}`);
   return req.json()
