54116aa0cc54a77622423daab184149b2ac31ef6
Ian Jamieson <cogocreative@gmail.com>
2023-01-11 15:22:01 +0100

    extend portal

--- src/ui/providers/portal/portal-renderer.tsx
+++ src/ui/providers/portal/portal-renderer.tsx
@@ -1,9 +1,8 @@
 import { component$ } from "@builder.io/qwik";
 import { IPortal, usePortal } from "~/ui/providers/portal/index";
 import { Box } from "~/ui/components/box";
-import { PortalsList } from "~/components/portals/portalsList";
 
-export const PortalRenderer = component$((props: { portalsList: PortalsList }) => {
+export const PortalRenderer = component$((props: { portalsList: any }) => {
   const state = usePortal();
   const portalsList = props.portalsList;
   return (
