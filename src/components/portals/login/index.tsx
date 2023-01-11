b75f752350e9851c5a71f23bc70e6a04c17378e0
Ian Jamieson <cogocreative@gmail.com>
2023-01-11 16:14:14 +0100

    overlay

--- src/components/portals/login/index.tsx
+++ src/components/portals/login/index.tsx
@@ -1,6 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 import { IPortal, usePortal } from "~/ui/providers/portal";
 import { Overlay } from "~/ui/components/overlay";
+import { Box } from "~/ui/components/box";
 
 export interface ILoginPortal extends IPortal {}
 
@@ -8,11 +9,11 @@ export const LoginPortal = component$((props: ILoginPortal) => {
   const portal = usePortal();
   return (
     <Overlay>
-      <aside>
+      <Box bg="white">
         <h1>{props.portalKey}</h1>
         <p>blah</p>
         <button onClick$={() => portal.closePortal(props.portalKey as any)}>Close</button>
-      </aside>
+      </Box>
     </Overlay>
   );
 });
