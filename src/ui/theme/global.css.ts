4491c7615d5e93cbb8915e076654462b599527c0
Ian Jamieson <cogocreative@gmail.com>
2023-01-13 14:41:09 +0100

    theme provider updates

--- src/ui/theme/global.css.ts
+++ src/ui/theme/global.css.ts
@@ -2,7 +2,8 @@ import { globalStyle } from '@vanilla-extract/css';
 
 globalStyle('html, body', {
   margin: 0,
-  padding: 0
+  padding: 0,
+  overflowX: 'hidden',
 });
 
 globalStyle('*, *:after, *:before', {
