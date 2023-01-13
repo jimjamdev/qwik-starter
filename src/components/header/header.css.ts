d561ccd6e7da48aae0b9d1830d200cefa46a59a9
Ian Jamieson <cogocreative@gmail.com>
2023-01-13 11:47:30 +0100

    manifest.json

--- src/components/header/header.css.ts
+++ src/components/header/header.css.ts
@@ -2,6 +2,9 @@ import { style } from "styled-vanilla-extract/qwik";
 import { theme } from '~/ui/theme/theme.css';
 
 export const headerCss = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
   backgroundColor: theme.color.brand,
   color: 'white',
   height: '60px',
