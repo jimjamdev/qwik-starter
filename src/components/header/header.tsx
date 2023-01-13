758b49369ce8c6ded4de960dacff64abd7c19d8c
Ian Jamieson <cogocreative@gmail.com>
2023-01-13 12:23:15 +0100

    manifest.json

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import { component$, useClientEffect$, useSignal, useStore } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import { Link } from '@builder.io/qwik-city';
 import { getLocale } from '~/utils/routing/getLocale';
 import { headerCss, linkStyle } from "~/components/header/header.css";
@@ -6,24 +6,12 @@ import { headerCss, linkStyle } from "~/components/header/header.css";
 export default component$(() => {
   const locale = getLocale();
   const path = `/${locale}/`;
-  const ref = useSignal<Element>();
-  const store = useStore({
-    height: 0,
-  });
-  useClientEffect$(() => {
-    console.log('ref', ref?.value);
-    if (ref.value) {
-      const rect = ref.value.getBoundingClientRect();
-      store.height = Math.round(rect.height);
-    }
-  });
 
   return (
-    <nav class={headerCss} ref={ref}>
+    <nav class={headerCss}>
       <Link class={linkStyle} href={`${path}`} aria-current="page">Home</Link>
 
       <Link class={linkStyle} href={`${path}listing`}>Listing</Link>
-      {store.height}
     </nav>
   );
 });
