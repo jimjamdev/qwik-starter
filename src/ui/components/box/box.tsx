59134d4a751f278ce0a7096fd4e9bb2a7c4a1ee5
Ian Jamieson <cogocreative@gmail.com>
2023-01-16 11:50:30 +0100

    button updates

--- src/ui/components/box/box.tsx
+++ src/ui/components/box/box.tsx
@@ -1,12 +1,7 @@
 import { rainbowSprinkles, Sprinkles } from "~/ui/theme/rainbow-sprinkles.css";
 import { component$, Slot } from "@builder.io/qwik";
 
-export interface BoxProps extends Sprinkles {
-  class?: string;
-  as?: any;
-  ref?: any;
-  onClick$?: () => void;
-}
+export type BoxProps = typeof rainbowSprinkles & Sprinkles & any;
 
 export const Box = component$((props: BoxProps) => {
   const { className, style, otherProps } = rainbowSprinkles(props);
