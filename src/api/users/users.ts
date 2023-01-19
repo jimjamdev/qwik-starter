2163ecb6fec8f0ca629d05f78f6082217f237d85
Ian Jamieson <cogocreative@gmail.com>
2023-01-19 04:12:28 +0100

    pkg update

--- src/api/users/users.ts
+++ src/api/users/users.ts
@@ -14,20 +14,33 @@ export interface Params {
   limit?: number | string;
   page?: number | string;
 }
+
+export const fetcher = async (
+  url: string,
+  params?: Params,
+  options?: RequestInit
+): Promise<any> => {
+  const res = await fetch(url, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json ", // set the header bearer
+      "Bearer ": "Bearer  1234567890", // set the header bearer
+    },
+  }); // include try cache
+  if (!res.ok) {
+    throw new Error(res.statusText);
+  }
+  return res.json();
+};
 //Test fetch
 export const fetchUsers = async (params: Params): Promise<Users> => {
   const { limit = 5, page = 1 } = params || {};
-  const req = await fetch(`https://catfact.ninja/breeds?limit=${limit}&page=${page}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Bearer': 'Bearer 1234567890',
-    },
-  });
-  return req.json()
+  return fetcher(`https://catfact.ninja/breeds?limit=${limit}&page=${page}`);
 };
 
 export const fetchIP = async (): Promise<IP> => {
-  const req = await fetch('https://api.ipify.org/?format=json');
-  return req.json()
+  const req = await fetch("https://api.ipify.org/?format=json");
+  return req.json();
 };
+
+
